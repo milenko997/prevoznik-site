cd923d61773474b1bcd3661fce9f31ecede9466e
Milenko Ilic <milenko.ilic997@gmail.com>
2026-04-01 15:48:12 +0200

    Creating services section

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Header from "@/components/header";
 import Footer from "@/components/footer";
 import Hero from "@/components/hero";
 import About from "@/components/about";
+import Services from "@/components/services";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <main className="main-content">
         <Hero />
         <About />
+        <Services />
       </main>
       <Footer />
     </>
